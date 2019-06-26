589c53cba6b62d28154c02a087165d0db19390d2
Rohan2599 <201651044@iiitvadodara.ac.in>
2019-06-26 21:23:49 +0530

    integrated authentication with frontend

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -1,10 +1,12 @@
+
+
 import React, { Component, Fragment } from "react";
 import { withAlert } from "react-alert";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-class Alerts extends Component{
- static propTypes = {
+export class Alerts extends Component {
+  static propTypes = {
     error: PropTypes.object.isRequired,
     message: PropTypes.object.isRequired
   };
@@ -16,27 +18,22 @@ class Alerts extends Component{
       if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`);
       if (error.msg.message)
         alert.error(`Message: ${error.msg.message.join()}`);
+      if (error.msg.non_field_errors)
+        alert.error(error.msg.non_field_errors.join());
+      if (error.msg.username) alert.error(error.msg.username.join());
     }
 
     if (message !== prevProps.message) {
       if (message.deleteLead) alert.success(message.deleteLead);
       if (message.addLead) alert.success(message.addLead);
+      if (message.passwordNotMatch) alert.error(message.passwordNotMatch);
     }
   }
 
-  render(){
-
-
-    return(
-      <Fragment />
-
-      )
+  render() {
+    return <Fragment />;
   }
-
-
 }
-
-
 
 const mapStateToProps = state => ({
   error: state.errors,
