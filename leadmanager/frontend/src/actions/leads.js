8227adedb6108f3e8e560f2a47f278cb921262bc
Rohan2599 <201651044@iiitvadodara.ac.in>
2019-06-27 14:01:37 +0530

    finished with the integration|

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,53 +1,50 @@
-import axios from 'axios';
-import {GET_LEADS, DELETE_LEAD,ADD_LEAD,CREATE_MESSAGE} from './types';
+import axios from "axios";
 import { createMessage, returnErrors } from "./messages";
+import { tokenConfig } from "./auth";
 
-export const getLeads = () => dispatch => {
-
-	axios.get('/api/leads')
-	.then(res=>{
-		dispatch({
-			type:GET_LEADS,
-			payload:res.data
-		})
-	})
-	.catch((err)=>{
-	 dispatch(returnErrors(err.response.data,err.response.status))
-	})
-}
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 
-
-export const deleteLead = id => dispatch => {
+export const getLeads = () => (dispatch, getState) => {
   axios
-    .delete(`/api/leads/${id}/`)
+    .get("/api/leads/", tokenConfig(getState))
     .then(res => {
-      dispatch(createMessage({deleteLead:"Lead Deleted"}))
+      dispatch({
+        type: GET_LEADS,
+        payload: res.data
+      });
+    })
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
+};
+
+
+export const deleteLead = id => (dispatch, getState) => {
+  axios
+    .delete(`/api/leads/${id}/`, tokenConfig(getState))
+    .then(res => {
+      dispatch(createMessage({ deleteLead: "Lead Deleted" }));
       dispatch({
         type: DELETE_LEAD,
         payload: id
       });
     })
-    .catch(err =>  console.log(err)
-
-      );
+    .catch(err => console.log(err));
 };
-//DELETE_LEAD
 
 
-export const addLead = lead => dispatch => {
+export const addLead = lead => (dispatch, getState) => {
   axios
-    .post("/api/leads/", lead)
+    .post("/api/leads/", lead, tokenConfig(getState))
     .then(res => {
-       dispatch(createMessage({addLead:"Lead Added"}));
+      dispatch(createMessage({ addLead: "Lead Added" }));
       dispatch({
         type: ADD_LEAD,
         payload: res.data
       });
     })
-    .catch(err =>{
-     
-     dispatch(returnErrors(err.response.data,err.response.status))
-
-    });
+    .catch(err =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
 };
